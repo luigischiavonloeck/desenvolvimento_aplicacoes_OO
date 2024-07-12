4a4f9543b0b52b2277806702f78b1dbe3adae02b
luigischiavonloeck <luigiloeck.pl008@academico.ifsul.edu.br>
2024-07-12 15:34:41 -0300

    atividade 3e4 base

---
+++ atividade_3e4/src/config/routes.php
@@ -0,0 +1,15 @@
+<?php
+
+use tsi\atividade_3e4\controller\api\{Posto, Bandeira, Usuario};
+//use tsi\atividade_3e4\controller\web\Posto as WebPosto;
+
+$routes = [
+    'api' => [
+        'postos' => Posto::class,
+        'bandeiras'=> Bandeira::class,
+        'usuarios'=> Usuario::class,
+    ],
+    // 'web' => [
+    //     'postos' => WebPosto::class
+    // ]
+];
---
+++ atividade_3e4/src/index.php
@@ -0,0 +1,6 @@
+<?php
+require '../vendor/autoload.php';
+
+use tsi\atividade_3e4\controller\App;
+
+App::init();
