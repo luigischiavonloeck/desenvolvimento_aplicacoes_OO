f380498e615ec723c50defd65251bf30533e580a
LuigiLoeck <luigisloeck@gmail.com>
2024-09-11 14:40:03 -0300

    atividade 6 queries done

--- atividade_6/routes/queries.php
+++ atividade_6/routes/queries.php
@@ -1,25 +1,42 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\{Posto, Bandeira, User};
+use App\Models\{Posto, Bandeira, Usuario};
 use Barryvdh\Debugbar\Facades\Debugbar;
 
 Route::prefix('queries')->group(function () {
 
-    // Query 1:
-    Route::get('',function(){});
+    // Carrega o posto com a sua bandeira
+    Route::get('posto-bandeira/{id}', function ($id) {
+        $result = Posto::find($id)->load('bandeira');
+        dump($result->toArray());
+        // return response()->json($result);
+    });
 
-    // Query 2:
-    Route::get('',function(){});
+    // Carrega o posto com os seus usuários
+    Route::get('posto-usuarios/{id}', function($id) {
+        $result = Posto::find($id)->load('usuarios');
+        dump($result->toArray());
+        // return response()->json($result);
+    });
 
-    // Query 3 (Com relacionamento):
-    Route::get('',function(){});
+    // Carrega a bandeira com os seus postos
+    Route::get('bandeira-postos/{id}', function($id) {
+        Debugbar::startMeasure('sql', 'Time to run query!!!');
+        $result = Bandeira::find($id)->load('postos');
+        Debugbar::stopMeasure('sql');
+        dump($result->toArray());
+        Debugbar::info($result->postos->count());
+    });
 
-    // Query 4 (Com relacionamento):
-    Route::get('',function(){});
+    // Retorna todos os postos que possuem usuários
+    Route::get('postos/usuarios/', function() {
+        dump(Posto::has('usuarios')->with('usuarios')->get()->toArray());
+    });
 
-    // Query 5 (Em varios niveis):
-    Route::get('',function(){});
-
+    // Retorna todas as bandeiras que possuem postos
+    Route::get('bandeiras/postos/', function() {
+        dump(Bandeira::has('postos')->with('postos')->get()->toArray());
+    });
 
 });
